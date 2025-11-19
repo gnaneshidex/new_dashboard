fd9c13f316000bfa3431d4426e64463a9852764e
gnaneshidex <gmalayamgajendran@idexcorp.com>
2025-11-19 10:05:23 -0500

    Update dashboard_new.py

--- dashboard_new.py
+++ dashboard_new.py
@@ -13,7 +13,7 @@ GITHUB_BASE = "https://raw.githubusercontent.com/gnaneshidex/TDS/main/Tool_Dashb
 DATA_DIR = "data"
 st.set_page_config(page_title="Tool Compatibility Optimizer", layout="wide")
 
-st.title("🔧 Tool Compatibility Optimizer — Ultra-Fast Dashboard")
+st.title("Akron Brass Compnay Tool Compatibility Optimizer")
 
 
 # =============================================
@@ -194,3 +194,4 @@ if st.button("⚙️ Optimize"):
             f,
             file_name=f"{selected_wc}_Optimization_Results.xlsx"
         )
+
